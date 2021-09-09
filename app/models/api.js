fec13d778aa561db6adcbb4e565388969066b4c0
kevin_smrt <kevinsanmartin@live.fr>
2021-09-09 20:35:21 +0200

    fixed error

--- app/models/api.js
+++ app/models/api.js
@@ -1,12 +1,15 @@
 // Récupère le module de l'api sendinblue
 const SibApiV3Sdk = require('sib-api-v3-sdk');
 
+// Récupère les variables du fichier .env
+require("dotenv").config();
+
 // Création du client par défaut
 const defaultClient = SibApiV3Sdk.ApiClient.instance;
 
 // Configuration de la clé API
 const apiKey = defaultClient.authentications['api-key'];
-apiKey.apiKey = "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"
+apiKey.apiKey = process.env.API_KEY;
 
 // Exportation de l'api
 module.exports = SibApiV3Sdk;
