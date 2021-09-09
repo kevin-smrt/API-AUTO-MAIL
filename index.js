1f5f641c15a755fee8b065f590ca926faad25813
kevin_smrt <kevinsanmartin@live.fr>
2021-09-09 17:10:54 +0200

    server ready to listen

---
+++ index.js
@@ -0,0 +1,17 @@
+// Récupère les variables du fichier .env
+require("dotenv").config();
+
+const express = require("express");
+const router = require("./app/router");
+
+// Utilise la variable PORT du fichier .env, si aucun fichier .env le port 3000 sera utilisé
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// Utilisation du router
+app.use(router);
+
+// Ecoute du serveur sur le port renseigné dans la variable PORT
+app.listen(PORT, () => {
+    console.log(`Server ready at http://localhost:${PORT}`);
+});
